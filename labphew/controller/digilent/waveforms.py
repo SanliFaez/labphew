73349bdf4ffbd846ab764a4900a697f915f82d2c
Aron Opheij <aron.opheij+github@gmail.com>
2020-06-15 17:12:38 +0200

    Added a read_simple_analog() method to digilent

--- labphew/controller/digilent/waveforms.py
+++ labphew/controller/digilent/waveforms.py
@@ -6,6 +6,7 @@ import dwf
 
 import time
 import matplotlib.pyplot as plt
+import numpy as np
 from ctypes import *
 
 
@@ -29,6 +30,44 @@ class DfwController(dwf.Dwf):
         self.di = self.DigitalIn
         self.do = self.DigitalOut
 
+        self._read_simple_analog_return_std = False
+
+
+    def preset_simple_analog(self, n=84, freq=10000, range=50.0, mult=(1.0, 1.0), return_std=False):
+        """
+        Apply settings for read_analog()
+        Please note that there may be a significant overhead (delay) which seems to be larger for lower frequencies
+        and oddly seems to be larger for collecting a small number of points.
+        The default values of averaging over 84 points at 10kHz results in 10ms per averaged datapoint.
+
+        :param n:     number of datapoints to collect and average (default 85)
+        :type n:      int
+        :param freq:  analog in frequency (default 10000)
+        :type freq:   int or float or None
+        :param range: the voltage range for the ADC (5.0 or 50.0) (default 50.0)
+        :type range:  int or float or None
+        :param return_std: also returns the standard deviations (default False)
+        :type return_std:  bool
+        """
+        daq.ai.reset()
+        self.ai.bufferSizeSet(n)
+        daq.ai.frequencySet(freq)
+        daq.ai.channelRangeSet(-1, range)
+        daq.ai.configure(False, True)  # apply config
+        time.sleep(0.1)
+
+    def read_simple_analog(self):
+        """Simplified method to read the two analog IN channels"""
+        daq.ai.configure(False, True)  # start acquisition
+        while daq.ai.status(True) != daq.ai.STATE.DONE:
+            pass
+        buf = daq.ai.bufferSizeGet()
+        c0 = np.array(daq.ai.statusData(0, buf))
+        c1 = np.array(daq.ai.statusData(1, buf))
+        if self._read_simple_analog_return_std:
+            return c0.mean(), c1.mean(), c0.std(), c1.std()
+        else:
+            return c0.mean(), c1.mean()
 
 
 def close_all():
@@ -50,13 +89,13 @@ def enumerate_devices():
         dev_dict['info']['userName'] = device.userName()
         dev_dict['dev'] = device
 
-        if not device.isOpened():
-            dwf_ai = dwf.DwfAnalogIn(device)
-            channel = dwf_ai.channelCount()
-            _, hzFreq = dwf_ai.frequencyInfo()
-            # print("\tAnalog input channels: " + str(channel))
-            dev_dict['info']['maxAIfreq'] = hzFreq
-            dwf_ai.close()
+        # if not device.isOpened():
+        dwf_ai = dwf.DwfAnalogIn(device)
+        channel = dwf_ai.channelCount()
+        _, hzFreq = dwf_ai.frequencyInfo()
+        # print("\tAnalog input channels: " + str(channel))
+        dev_dict['info']['maxAIfreq'] = hzFreq
+        dwf_ai.close()
 
         n_configs = dwf.FDwfEnumConfig(i)
         for iCfg in range(0, n_configs):
@@ -97,104 +136,88 @@ if __name__ == '__main__':
     devices = enumerate_devices()
     print_device_list(devices)
 
-    # create object with
+    # create object for device number 0, with config number 0
     daq = DfwController(0, 0)
 
+    if False:
+        print("\nConfigure and first analog out channel")
+        ch_out = 0
 
-    # daq.ao.nodeFunctionSet(ch, daq.ao.NODE.CARRIER, daq.ao.FUNC.DC)
-    # daq.ao.nodeFunctionSet(ch, daq.ao.NODE.CARRIER, daq.ao.FUNC.SINE)
+        print('Carrier: "sine", 0.4V, 6kz, offset 1V')
+        daq.ao.nodeEnableSet(ch_out, daq.ao.NODE.CARRIER, True)
+        daq.ao.nodeFunctionSet(ch_out, daq.ao.NODE.CARRIER, daq.ao.FUNC.SINE)
+        daq.ao.nodeFrequencySet(ch_out, daq.ao.NODE.CARRIER, 6000.0)
+        daq.ao.nodePhaseSet(ch_out, daq.ao.NODE.CARRIER, 0)
+        daq.ao.nodeAmplitudeSet(ch_out, daq.ao.NODE.CARRIER, 0.4)
+        daq.ao.nodeOffsetSet(ch_out, daq.ao.NODE.CARRIER, 1.0)
+
+        print('Amplitude Modulation: "ramp up", 400Hz, 100%')
+        daq.ao.nodeEnableSet(ch_out, daq.ao.NODE.AM, True)
+        daq.ao.nodeFunctionSet(ch_out, daq.ao.NODE.AM, daq.ao.FUNC.RAMP_UP)
+        daq.ao.nodeFrequencySet(ch_out, daq.ao.NODE.AM, 400.0)
+        daq.ao.nodePhaseSet(ch_out, daq.ao.NODE.AM, 0)
+        daq.ao.nodeAmplitudeSet(ch_out, daq.ao.NODE.AM, 100)
+
+        print('Frequency Modulation: "square", 100Hz, 20%, phase 90 degrees')
+        daq.ao.nodeEnableSet(ch_out, daq.ao.NODE.FM, True)
+        daq.ao.nodeFunctionSet(ch_out, daq.ao.NODE.FM, daq.ao.FUNC.SQUARE)
+        daq.ao.nodeFrequencySet(ch_out, daq.ao.NODE.FM, 100.0)
+        daq.ao.nodePhaseSet(ch_out, daq.ao.NODE.FM, 90)
+        daq.ao.nodeAmplitudeSet(ch_out, daq.ao.NODE.FM, 20)
+
+        print("\nConfigure analog in")
+        print('Sampling rate 100kHz, 1000 points (i.e. 10ms)')
+        n_points = 1000
+        daq.ai.frequencySet(1e5)
+        print("Set range for all channels")
+        daq.ai.channelRangeSet(-1, 4.0)
+        daq.ai.bufferSizeSet(n_points)
+        daq.ai.channelAttenuationSet(0, 0.5)
+
+        print("\nStarting output and starting acquisition")
+        daq.ao.configure(ch_out, True)
+        daq.ai.configure(True, True)
+
+        # print("   waiting to finish")
+        # while True:
+        #     if daq.ai.status(True) == daq.ai.STATE.DONE:
+        #         break
+        #     time.sleep(0.1)
+        # print("   done")
+
+        timeout = 0.1 + 16400 / daq.ai.frequencyGet()
+        t0 = time.time()
+        while daq.ai.status(True) != daq.ai.STATE.DONE:
+            time.sleep(0.001)
+            if time.time() > t0 + timeout:
+                print('timeout occured')
+                break
 
 
-    print("\nConfigure and first analog out channel")
+
+        print("   reading data")
+        rg = daq.ai.statusData(0, n_points)
+
+        # hdwf.close()  # DONT CLOSE, I WANT TO EXPLORE
+
+        dc = sum(rg) / len(rg)
+        print("DC: " + str(dc) + "V")
+
+        plt.plot(rg)
+        plt.show()
+
+
+    # example showing read_simple_analog()
+
     ch_out = 0
-
-    print('Carrier: "sine", 0.4V, 6kz, offset 1V')
-    daq.ao.nodeEnableSet(ch_out, daq.ao.NODE.CARRIER, True)
-    daq.ao.nodeFunctionSet(ch_out, daq.ao.NODE.CARRIER, daq.ao.FUNC.SINE)
-    daq.ao.nodeFrequencySet(ch_out, daq.ao.NODE.CARRIER, 6000.0)
-    daq.ao.nodePhaseSet(ch_out, daq.ao.NODE.CARRIER, 0)
-    daq.ao.nodeAmplitudeSet(ch_out, daq.ao.NODE.CARRIER, 0.4)
-    daq.ao.nodeOffsetSet(ch_out, daq.ao.NODE.CARRIER, 1.0)
-
-    print('Amplitude Modulation: "ramp up", 400Hz, 100%')
-    daq.ao.nodeEnableSet(ch_out, daq.ao.NODE.AM, True)
-    daq.ao.nodeFunctionSet(ch_out, daq.ao.NODE.AM, daq.ao.FUNC.RAMP_UP)
-    daq.ao.nodeFrequencySet(ch_out, daq.ao.NODE.AM, 400.0)
-    daq.ao.nodePhaseSet(ch_out, daq.ao.NODE.AM, 0)
-    daq.ao.nodeAmplitudeSet(ch_out, daq.ao.NODE.AM, 100)
-
-    print('Frequency Modulation: "square", 100Hz, 20%, phase 90 degrees')
-    daq.ao.nodeEnableSet(ch_out, daq.ao.NODE.FM, True)
-    daq.ao.nodeFunctionSet(ch_out, daq.ao.NODE.FM, daq.ao.FUNC.SQUARE)
-    daq.ao.nodeFrequencySet(ch_out, daq.ao.NODE.FM, 100.0)
-    daq.ao.nodePhaseSet(ch_out, daq.ao.NODE.FM, 90)
-    daq.ao.nodeAmplitudeSet(ch_out, daq.ao.NODE.FM, 20)
-
-    print("\nConfigure analog in")
-    print('Sampling rate 100kHz, 1000 points (i.e. 10ms)')
-    n_points = 1000
-    daq.ai.frequencySet(1e5)
-    print("Set range for all channels")
-    daq.ai.channelRangeSet(-1, 4.0)
-    daq.ai.bufferSizeSet(n_points)
-    daq.ai.channelAttenuationSet(0, 0.5)
-
-    print("\nStarting output and starting acquisition")
+    daq.ao.configure(ch_out, 0)
+    daq.ao.reset()
+    daq.ao.nodeFunctionSet(ch_out, daq.ao.NODE.CARRIER, daq.ao.FUNC.DC)
+    daq.ao.nodeOffsetSet(ch_out, daq.ao.NODE.CARRIER, -0.5)
+    print("\nStarting output")
     daq.ao.configure(ch_out, True)
-    daq.ai.configure(True, True)
+    time.sleep(0.1)  # allow output to stabilize
 
-    # print("   waiting to finish")
-    # while True:
-    #     if daq.ai.status(True) == daq.ai.STATE.DONE:
-    #         break
-    #     time.sleep(0.1)
-    # print("   done")
-
-    timeout = 0.1 + 16400 / daq.ai.frequencyGet()
-    t0 = time.time()
-    while daq.ai.status(True) != daq.ai.STATE.DONE:
-        time.sleep(0.001)
-        if time.time() > t0 + timeout:
-            print('timeout occured')
-            break
-
-
-
-    print("   reading data")
-    rg = daq.ai.statusData(0, n_points)
-
-    # hdwf.close()  # DONT CLOSE, I WANT TO EXPLORE
-
-    dc = sum(rg) / len(rg)
-    print("DC: " + str(dc) + "V")
-
-    plt.plot(rg)
-    plt.show()
-
-    # daq.ao.configure(ch_out, 0)
-    # daq.ao.reset()
-    # daq.ao.nodeFunctionSet(ch_out, daq.ao.NODE.CARRIER, daq.ao.FUNC.DC)
-    # daq.ao.nodeOffsetSet(ch_out, daq.ao.NODE.CARRIER, -0.7)
-    # print("\nStarting output")
-    # daq.ao.configure(ch_out, True)
-    #
-    # n_points = 10
-    # daq.ai.frequencySet(1000)
-    # print("Set range for all channels")
-    # daq.ai.channelRangeSet(-1, 4.0)
-    # daq.ai.bufferSizeSet(n_points)
-    #
-    # print("\nStarting input")
-    # daq.ai.configure(True, True)
-    #
-    # while daq.ai.status(True):
-    #     time.sleep(0.001)
-    #
-    # print("   reading data")
-    # rg = daq.ai.statusData(0, n_points)
-    # print(rg)
-
-
-# TODO:
-# attenuation analog in
-# single shot ???
+    daq.preset_simple_analog()
+    time.sleep(1)
+    print(daq.read_simple_analog())
