6eef06f907ca67c7e8b795455985bf64f401c98e
Aron Opheij <aron.opheij+github@gmail.com>
2020-07-27 16:56:37 +0200

    Created a purely simulated Blink controller

--- labphew/controller/blink_controller.py
+++ labphew/controller/blink_controller.py
@@ -1,158 +1,96 @@
 # -*- coding: utf-8 -*-
 """
-==================
-Example controller
-==================
+================
+Blink controller
+================
 
-This is an example of a controller with a fake (invented) device. It should help to gide
+This is an example of a controller with a fake (invented) device. It should help to guide
 developers to create new controllers for real devices.
 
 
 """
-from hyperion.core import logman as logging
-from hyperion.controller.base_controller import BaseController
+import logging
+import time
 
-class ExampleController(BaseController):
-    """ Example output device that does not connect to anything"""
-
-    FAKE_RESPONSES = {'A': 1,
-                     }
-
-    def __init__(self, settings):
-        """ Init of the class.
-
-        :param settings: this includes all the settings needed to connect to the device in question.
-        :type settings: dict
-
+class BlinkController:
+    """
+    Blink Controller: Fake Device to act as an example
+    """
+    def __init__(self, properties = {}):
         """
-        super().__init__(settings)  # mandatory line
+
+        :param properties: optional properties dictionary
+        :type properties: dict
+        """
         self.logger = logging.getLogger(__name__)
-        self.logger.info('Class ExampleController created.')
-        self.name = 'Example Controller'
 
-        self._amplitude = []
+        # Set parameters to simulate the device
+        self.__simulated_device_blink_period= 1  #
+        self.__simulated_device_start_time = time.time()
+        self.__simulated_device_status = False
 
-    def initialize(self):
-        """ Starts the connection to the device in port """
-        self.logger.info('Opening connection to device ExampleController (a fake connection)')
-        self._is_initialized = True     # THIS IS MANDATORY!!
-                                        # this is to prevent you to close the device connection if you
-                                        # have not initialized it inside a with statement
-        self._amplitude = self.query('A?')
-
-    def finalize(self):
-        """ This method closes the connection to the device.
-        It is ran automatically if you use a with block
-
-        """
-        self.logger.info('Closing connection to device.')
-        self._is_initialized = False
-
-    def idn(self):
-        """ Identify command
-
-        :return: identification for the device
-        :rtype: string
-        """
-        self.logger.debug('Ask IDN to device.')
-        return 'ExampleController device'
-
-    def query(self, msg):
-        """ writes into the device msg
-
-        :param msg: command to write into the device port
-        :type msg: string
-        """
-        self.logger.debug('Writing into the example device:{}'.format(msg))
-        self.write(msg)
-        ans = self.read()
-        return ans
-
-    def read(self):
-        """ Fake read that returns always the value in the dictionary FAKE RESULTS.
-        
-        :return: fake result
-        :rtype: string
-        """
-        return self.FAKE_RESPONSES['A']
-
-    def write(self, msg):
-        """ Writes into the device
-        :param msg: message to be written in the device port
-        :type msg: string
-        """
-        self.logger.debug('Writing into the device:{}'.format(msg))
-
-
-    @property
-    def amplitude(self):
-        """ Gets the amplitude value.
-
-        :getter:
-        :return: amplitude value in Volts
-        :rtype: float
-        :setter:
-        :param value: value for the amplitude to set in Volts
-        :type value: float
-
-        For example, to use the getter you can do the following
-        ampl = this_controller.amplitude
-        For example, using the setter looks like this:
-        this_controller.amplitude = 5
-
-        """
-        self.logger.debug('Getting the amplitude.')
-        return self._amplitude
-
-    @amplitude.setter
-    def amplitude(self, value):
-        # would be nice to add a way to check that the value is within the limits of the device.
-        if self._amplitude != value:
-            self.logger.info('Setting the amplitude to {}'.format(value))
-            self._amplitude = value
-            self.write('A{}'.format(value))
+        # Set user parameters. Optionally get them from properties. Otherwise use hardcoded values:
+        if 'max_blink_period' in properties:
+            self.max_blink_period = properties['max_blink_period']
         else:
-            self.logger.info('The amplitude is already {}. Not changing the value in the device.'.format(value))
+            self.max_blink_period = 2
 
+        if 'min_blink_period' in properties:
+            self.min_blink_period = properties['min_blink_period']
+        else:
+            self.min_blink_period = 0.2
 
-class ExampleControllerDummy(ExampleController):
-    """
-    Example Controller Dummy
-    ========================
+        self.logger.debug('BlinkController object created')
 
-    A dummy version of the Example Controller.
-
-    In essence we have the same methods and we re-write the query to answer something meaningful but
-    without connecting to the real device.
-
-    """
-
-
-    def query(self, msg):
-        """ writes into the device msg
-
-        :param msg: command to write into the device port
-        :type msg: string
+    def set_blink_period(self, period_s):
         """
-        self.logger.debug('Writing into the dummy device:{}'.format(msg))
-        ans = 'A general dummy answer'
-        return ans
+        Method that mimics setting a device parameter.
 
 
+        :param period_s:
+        :type period_s:
+        :return:
+        :rtype:
+        """
+        # You could do some checks first. For example to see if the value is in an allowed range:
+        if period_s > self.max_blink_period:
+            self.logger.warning(f'Blink period of {period_s}s exceeds maximum allowed. Setting period to {self.max_blink_period}s')
+            period_s = self.max_blink_period
+        if period_s < self.min_blink_period:
+            self.logger.warning(f'Blink period of {period_s}s exceeds minimum allowed. Setting period to {self.min_blink_period}s')
+            period_s = self.min_blink_period
+        # Your code to communicate with the device goes here.
+        # For the purpose of demonstration, this method simulates setting a parameter on a device:
+        self.logger.debug('"Sending" blink period of {} to device'.format(period_s))
+        self.__simulated_device_blink_period = period_s
+        self.__simulated_device_start_time = time.time()
+        self.__simulated_device_status = not self.__simulated_device_status
+
+    def get_status(self):
+        """
+        Method that mimics communicating with a device and retrieving a status.
+
+        :return: Device is "on"
+        :rtype: bool
+        """
+        # Your code to communicate with the device goes here.
+        # For the purpose of demonstration, this method returns a simulated status:
+        self.logger.debug('"Retrieving" status from device')
+        return bool(int((time.time()-self.__simulated_device_start_time)/self.__simulated_device_blink_period/.5) % 2)
 
 if __name__ == "__main__":
+    import labphew
 
-    dummy = False  # change this to false to work with the real device in the COM specified below.
+    device = BlinkController()
+    print(device.get_status())
 
-    if dummy:
-        my_class = ExampleControllerDummy
-    else:
-        my_class = ExampleController
+    device.set_blink_period(3)
+    device.set_blink_period(0.1)
 
-    with my_class(settings  = {'port':'COM10', 'dummy': False}) as dev:
-        dev.initialize()
-        print(dev.amplitude)
-        dev.amplitude = 5
-        print(dev.amplitude)
-
-
+    # import matplotlib.pyplot as plt
+    # device.logger.setLevel(logging.INFO)
+    # record = []
+    # for i in range(100):
+    #     time.sleep(0.01)
+    #     record.append(device.get_status())
+    # plt.plot(record)
