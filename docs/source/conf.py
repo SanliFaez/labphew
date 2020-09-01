44b3cb63eda2364e86909f7d14cb356562142be6
Aron Opheij <aron.opheij+github@gmail.com>
2020-09-01 14:19:13 +0200

    api doc test: added controller directory exlicitly to sys path insert

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath('.')), 'controller'))
 
 # -- General configuration ------------------------------------------------
 
